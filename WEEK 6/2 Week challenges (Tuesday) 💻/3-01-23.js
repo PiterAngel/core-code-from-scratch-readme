f46898204baf73c16446a24eadd89712666b7e26
Peter Angel Valiente De León <valientedeleonp@gmail.com>
2023-01-07 16:18:53 -0600

    actualizanding

---
+++ WEEK 6/2 Week challenges (Tuesday) 💻/3-01-23.js
@@ -0,0 +1,30 @@
+// ======= VARIABLES =======
+let firstname = "Lata";
+console.log(firstname);
+
+// ======= What is x? =======
+let x = "Hola Mundo";
+console.log(x);
+
+// ======= Several variables =======
+let food = "Pizza";
+const valorpi = "3.1416";
+var edad = 23;
+console.log("My favorite food is:", food);
+console.log("The value of Pi is: ", valorpi);
+console.log("My age is: ", edad)
+
+// ======= Reassignment =======
+let a = "Tic";
+a = "Toc";
+a = "Toe";
+console.log(a)
+
+// ======= Assign variables =======
+let b = "Laurel";
+let c = "Hardy";
+let d = c;
+c = b;
+b = d;
+console.log(c)
+console.log(b)
