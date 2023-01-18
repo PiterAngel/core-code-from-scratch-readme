7f349861b6305d749bb0705d90dea6f46f2b2b2c
Peter Angel Valiente De León <valientedeleonp@gmail.com>
2023-01-17 21:48:23 -0600

    actualizanding

--- Week 8/16 Week challenges (Monday) 💻/16-01-23.js
+++ Week 8/16 Week challenges (Monday) 💻/16-01-23.js
@@ -1,6 +1,6 @@
-let total = 0;
-
-function hotdog(n) {
+// ======= Training JS #7: if..else and ternary operator! =======
+function saleHotdogs(n) {
+    let total = 0;
     if (n < 5) {
         total += 100 * n
     } else if (n >= 5 && n < 10) {
@@ -10,3 +10,65 @@ function hotdog(n) {
     }
     return total;
 }
+console.log(saleHotdogs(5))
+
+// ====== Training JS #8: Conditional statement--switch =======
+function howManydays(month) {
+    var days;
+    switch (month) {
+        case 1:
+            days = 31
+            break;
+        case 2:
+            days = 28
+            break;
+        case 3:
+            days = 31;
+            break;
+        case 4:
+            days = 30;
+            break
+        case 5:
+            days = 31;
+            break;
+        case 6:
+            days = 30;
+            break;
+        case 7:
+            days = 31;
+            break;
+        case 8:
+            days = 31;
+            break;
+        case 9:
+            days = 30;
+            break;
+        case 10:
+            days = 31;
+            break;
+        case 11:
+            days = 30;
+            break;
+        case 12:
+            days = 31;
+            break;
+    }
+    return days;
+}
+console.log(howManydays(1));
+
+
+// ======= Basic Calculator =======
+function calculate(num1, operation, num2) {
+    if (operation === "+") {
+        return num1 + num2;
+    } else if (operation === "-") {
+        return num1 - num2
+    } else if (operation === "*") {
+        return num1 * num2
+    } else if (operation === "/" && num2 != 0) {
+        return num1 / num2
+    } else
+        return null
+}
+console.log(calculate(1, "+", 1))
---
+++ Week 8/17 Week challenges (Tuesday) 💻/17-01-23.js
@@ -0,0 +1,9 @@
+// ======= Even or odd =======
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return 'Even';
+    }
+    return 'Odd';
+}
+
+// ====== A wolf in sheep's clothing ======
